7b6437bcfe045dfe131c8ed838a68ca0205a28b2
Phil Schumann <metaleap@users.noreply.github.com>
2023-11-11 19:42:06 +0100

    vsChat cont. #3

--- ui/webviews/chat_providers.ts
+++ ui/webviews/chat_providers.ts
@@ -1,12 +1,25 @@
+import * as util from './util'
 import * as yo from './yo-sdk'
-
-export type ServerChannel = {
-    name: string,
-    readOnly: boolean,
-}
+import * as youtil from './yo-util'
 
 export type ServerImpl = {
-    channels: () => Promise<ServerChannel[]>,
+    curChannelId: number,
+    channels: ServerChannel[],
+    loadChannelsList: () => void
+}
+
+export type ServerChannel = {
+    id: number,
+    title: string,
+    readOnly: boolean,
+    posts: ChannelPost[],
+    pollingPaused: boolean,
+    pollingStart: () => void,
+    onFreshPosts: (() => void) | null,
+}
+
+export type ChannelPost = {
+    id: number,
 }
 
 export function newTwitch(): ServerImpl {
@@ -14,17 +27,65 @@ export function newTwitch(): ServerImpl {
 }
 
 export function newKaffe(): ServerImpl {
-    const ret: ServerImpl = {
-        channels: async (): Promise<ServerChannel[]> => {
-            const ret: ServerChannel[] = [{
-                name: "(all buddies)",
+    let ret_impl: ServerImpl
+    let next_since: string | undefined = undefined
+    const do_poll_now = async () => {
+        const channel = ret_impl.channels.find(_ => (_.id === ret_impl.curChannelId))!
+        if (channel.pollingPaused)
+            return
+        try {
+            const results = await yo.api__postsRecent({
+                OnlyBy: (ret_impl.curChannelId ? [ret_impl.curChannelId] : []),
+                Since: next_since,
+            })
+            if (channel.pollingPaused)
+                return
+            next_since = results.NextSince
+            let fresh_posts: ChannelPost[] = results.Posts.map(_ => {
+                return { id: _.Id! }
+            })
+            for (const old_post of channel.posts)
+                if (!fresh_posts.some(_ => (_.id === old_post.id)))
+                    fresh_posts.push(old_post)
+            if (!youtil.deepEq(fresh_posts, channel.posts)) {
+                channel.posts = fresh_posts
+                if (channel.onFreshPosts)
+                    channel.onFreshPosts()
+            }
+        } catch (err) {
+            if (!channel.pollingPaused)
+                util.alert(youtil.errStr(err))
+        }
+
+        if (!channel.pollingPaused)
+            setTimeout(do_poll_now, 4321)
+    }
+
+    ret_impl = {
+        curChannelId: 0,
+        channels: [],
+        loadChannelsList: async () => {
+            ret_impl.channels = [{
+                id: 0,
+                title: "(all buddies)",
                 readOnly: false,
+                posts: [],
+                pollingPaused: true,
+                onFreshPosts: null,
+                pollingStart: async () => { ret_impl.curChannelId = 0; do_poll_now(); },
             }]
             const buddies = (await yo.api__userBuddies({})).Buddies
             for (const buddy of buddies)
-                ret.push({ name: buddy.Nick!, readOnly: false })
-            return ret
-        }
+                ret_impl.channels.push({
+                    id: 0,
+                    title: buddy.Nick!,
+                    readOnly: false,
+                    posts: [],
+                    pollingPaused: true,
+                    onFreshPosts: null,
+                    pollingStart: async () => { ret_impl.curChannelId = buddy.Id!; do_poll_now() },
+                })
+        },
     }
-    return ret
+    return ret_impl
 }
